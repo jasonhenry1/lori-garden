3ef9578213bb749cbbd7854672db6324a7edcbc6
jasonhenry1 <87250896+jasonhenry1@users.noreply.github.com>
2021-07-28 15:17:03 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-//Image slider 
-document.addEventListener( 'DOMContentLoaded', function () {
-	new Splide( '.splide', {
-        lazyLoad: 'nearby',
-        heightRatio: 0.3,
-        cover: true,
-        width: "100%",
-	} ).mount();
-} );
