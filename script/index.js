d3780fc6256195a342f9aea130b7af3d8d2ea02b
jasonhenry1 <87250896+jasonhenry1@users.noreply.github.com>
2021-07-28 15:35:14 -0500

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,9 @@
+// EXTERNAL: Splide slider
+document.addEventListener( 'DOMContentLoaded', function () {
+		new Splide( '.splide', {
+			type		:	'loop',
+			heightRatio	:	0.3,
+			autoplay	:	true,
+		} ).mount();
+	} );
+
