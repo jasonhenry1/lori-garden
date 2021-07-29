8b76c91172a19cbb853118837b7076fa3e5b01e0
jasonhenry1 <87250896+jasonhenry1@users.noreply.github.com>
2021-07-29 00:56:25 -0500

    Delete script directory

--- script/index.js
+++
@@ -1,9 +0,0 @@
-// EXTERNAL: Splide slider
-document.addEventListener( 'DOMContentLoaded', function () {
-		new Splide( '.splide', {
-			type		:	'loop',
-			heightRatio	:	0.3,
-			autoplay	:	true,
-		} ).mount();
-	} );
-
